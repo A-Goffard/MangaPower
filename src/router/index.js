e7076765e122b1528611ba5ed5dce193fd919843
A-Goffard <aintzagoffard@gmail.com>
2024-03-26 14:27:43 +0100

    rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Loging from '../views/register/Loging.vue'
+import Register from '../views/register/Register.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +19,17 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/loging',
+      name: 'Loging',
+      component: Loging,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+    },
   ]
 })
 
---
+++ src/views/register/Loging.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="contenedor-general">
+        <article>
+            <input type="text" id="nombre">
+            <input type="text" id="apellido">
+            <input type="password" id="password">
+            <input type="password" id="passwordComprobation">
+
+            <div>
+                <input class="botoncillo" type="checkbox" name="checkbox" id="botoncillo"><span>Acepto la <a href="#">política de privacidad</a></span>
+            </div>
+            <button id="boton" class="desactivado">Enviar</button>
+        </article>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style >
+
+</style>
