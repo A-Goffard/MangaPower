9bd190ae5cfa002b14632c38794dc9ef9b4781ca
A-Goffard <aintzagoffard@gmail.com>
2024-03-27 14:23:22 +0100

    logigng

--- src/views/LogingView.vue
+++ src/views/LogingView.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
       <h1>LOGING</h1>
+      <article>
+        <div><span>Name </span><input type="text" id="name"></div>
+        <div><span>Birthdate </span><input type="text" id="birthdate"></div>
+        <div><span>Mail </span><input type="text" id="mail"></div>
+        <div><span>UserName </span><input type="text" id="userName"></div>
+        <div><span>Password </span><input type="text" id="password"></div>
+        <div><span>Password </span><input type="text" id="passwordComprobation"></div>
+        <div>
+            <input class="checkbox" type="checkbox" name="checkbox" id="botoncillo"><span> Acepto la <a href="#">política de privacidad</a></span>
+        </div>
+        <button id="register" class="desactivado">Register</button>
+    </article>
     </div>
   </template>
   
