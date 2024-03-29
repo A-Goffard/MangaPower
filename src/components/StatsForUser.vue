75deeb8e142d4a6f4eaffbab5ba01704f24f08be
johanrodriguez24 <srodriguez5860@gmail.com>
2024-03-29 15:43:24 +0100

    Ajustes al estilo dentro de las estadísticas

--- src/components/StatsForUser.vue
+++ src/components/StatsForUser.vue
@@ -2,10 +2,15 @@
 
 <div class="containerStats">
 
+    <div class="informationStats">
+        <h3>Name</h3>
+        <h3>Avatar</h3>
+        <h3>Stats</h3>
+    </div>
 
 </div>
-<div class="informationStats">
-    <h2>PJ "Partidas jugadas" PG "Partidas ganadas" PP "Partidas perdidas"</h2>
+<div class="informationTittle">
+    <h2>PJ "Partidas jugadas" // PG "Partidas ganadas" // PP "Partidas perdidas"</h2>
 </div>
 
 
@@ -19,7 +24,25 @@
     background-image: url('/public/images/1366_2000.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+    position: relative;
     
+}
+
+.informationStats{
+    width: 33rem;
+    height: 18rem;
+    border: 0.1rem solid red;
+    position: absolute;
+    left: 36%;
+    top: 40%;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+}
+.informationStats h3{
+    margin: 0.5rem;
 }
 
 
