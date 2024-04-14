ce2b977ab8eca7723261c399c6e480261d16648b
A-Goffard <aintzagoffard@gmail.com>
2024-04-14 20:28:32 +0200

    teses e2e empezando y el primero activo y correcto 1

--- MangaPowerFront/dist/js/about.8607ddb2.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmangapower"]=self["webpackChunkmangapower"]||[]).push([[594],{657:function(n,a,u){u.r(a),u.d(a,{default:function(){return l}});var e=u(641);const r={class:"about"},t=(0,e.Lk)("h1",null,"This is an about page",-1),s=[t];function c(n,a){return(0,e.uX)(),(0,e.CE)("div",r,s)}var o=u(262);const i={},f=(0,o.A)(i,[["render",c]]);var l=f}}]);
-//# sourceMappingURL=about.8607ddb2.js.map
