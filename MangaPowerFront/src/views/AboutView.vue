ae9beca2a5d5a5accc61b4a48bedd8968c649f55
A-Goffard <aintzagoffard@gmail.com>
2024-04-18 09:56:07 +0200

    inserted video

--- MangaPowerFront/src/views/AboutView.vue
+++ MangaPowerFront/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="mainDivHome">
+    <div class="logo">
+      <img class="logoimage" src="logo.png" alt="">
+    </div>
     <div class="about-page">
       <h1>Bienvenido a Pokémon Cards Adventure</h1>
       <div class="section">
@@ -33,8 +36,8 @@
 
 <style>
 
-.mainDivHome{
-  height: 100vh;
+.mainDivHome {
+  height: auto; /* Ajustar la altura automáticamente */
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -42,7 +45,18 @@
   background-image: url('/public/autumn-night-illuminated-lantern-tree-yellow-leaf-generated-by-ai.jpg'); 
   background-size: cover; 
   background-repeat: no-repeat; 
-
+  background-attachment: fixed; /* Fija el fondo */
+}
+.logoimage {
+  width: 100%;
+}
+.logo {
+  max-width: 30rem;
+  width: 70%;
+  padding: 0rem 1rem;
+  margin: 1rem;
+  background-color: rgb(255, 255, 255, 0.65);
+  border-radius: 0.5rem;
 }
 .about-page {
   padding: 20px;
