6133f6ea76f960ab1a0a7cc857d80d0bc7acc4b4
A-Goffard <aintzagoffard@gmail.com>
2024-04-20 16:55:22 +0200

    winedCards xxxx

--- MangaPowerFront/src/components/AvatarUser.vue
+++ MangaPowerFront/src/components/AvatarUser.vue
@@ -1,24 +1,25 @@
 <template>
 
-          <div class="avatarContainerUser">
-            <img class="avatarUser" :src="userData && `avatar/${userData.pokemonTrainer}.png`" alt="Avatar del entrenador">
-          </div>
- 
-  </template>
+  <div class="avatarContainer">
+    <img class="avatar" :src="userData && `avatar/${userData.pokemonTrainer}.png`" alt="Avatar del entrenador">
+  </div>
+
+</template>
 
 <style scoped>
-.avatarContainerUser {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: min-content;
-  padding: 1rem;
-  margin: 1rem;
-  border-radius: 0.5rem;
-} 
-.avatarUser {
-    max-width: 20rem;
+.avatarContainer {
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: min-content;
+padding: 1rem;
+margin: 1rem;
+background-color: rgb(255, 255, 255, 0.65);
+border-radius: 0.5rem;
+}
+.avatar {
+max-width: 20rem;
 }
 
 </style>
@@ -31,11 +32,11 @@ let userData = ref(null);
 
 // Función para obtener los datos del usuario del localStorage
 const getUsuarioFromLocalStorage = () => {
-  const usuario = JSON.parse(localStorage.getItem('usuario'));
-  if (usuario && usuario.length > 0) {
-    // Si hay datos de usuario en el localStorage, asignar el primer elemento del array a userData
-    userData.value = usuario[0];
-  }
+const usuario = JSON.parse(localStorage.getItem('usuario'));
+if (usuario && usuario.length > 0) {
+// Si hay datos de usuario en el localStorage, asignar el primer elemento del array a userData
+userData.value = usuario[0];
+}
 };
 
 // Llamar a la función para obtener los datos del usuario al montar el componente
